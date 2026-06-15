253e17a164d436cb4fc21a72b04ff0e818942e40
badrulhusain <badru773dhdc@gmail.com>
2026-06-15 17:17:30 +0530

    feat: add isActive field to CreateCategoryDto and CreateTagDto with validation, and update services to handle it

--- src/tags/dto/create-tag.dto.ts
+++ src/tags/dto/create-tag.dto.ts
@@ -1,8 +1,18 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsBoolean,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateTagDto {
   @IsString()
   @MinLength(2)
   @MaxLength(60)
   name: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
 }
